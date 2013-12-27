d68d618de5da5de695bacf7a59fc53c12e9140a5
juacompe <juacompe@gmail.com>
2013-12-27 21:42:39 +0700

    add a test that the service is listening at port 80

--- test/integration/default/serverspec/wonderstuff-design.me/default_spec.rb
+++ test/integration/default/serverspec/wonderstuff-design.me/default_spec.rb
@@ -12,6 +12,10 @@ describe "Wonderstuff Design" do
         it { should be_running }
     end
 
+    it "should listen at port 80" do
+        expect(port(80)).to be_listening
+    end
+
     it "should render the Wonderstuff Design web page" do
         expect(file('/var/www/index.html')).to be_file
         expect(file('/var/www/index.html')).to contain "Wonderstuff Design is a boutique graphics design agency." 
